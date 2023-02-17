85b3ec28fca5d23a0e152246f58b22673990f637
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-02-17 12:25:51 +0530

    Update unzip_help.py

--- unzipper/helpers/unzip_help.py
+++ unzipper/helpers/unzip_help.py
@@ -37,7 +37,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         )
         try:
             await message.edit(
-                text="{}\n {} \n\n**Powered by @EDM115bots**".format(ud_type, tmp)
+                text="{}\n {} \n\n**Powered by @BOTXZE**".format(ud_type, tmp)
             )
         except:
             pass
